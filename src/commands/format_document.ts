8f0915bd2e4a8dbe85d3a2fded5a2d80c31800b5
Sam Gwilym <gwilym@me.com>
2022-01-27 14:38:15 +0100

    Fix non-awaited applying of LSP edits

--- src/commands/format_document.ts
+++ src/commands/format_document.ts
@@ -40,6 +40,6 @@ export default function registerFormatDocument(client: LanguageClient) {
       return;
     }
 
-    applyLSPEdits(editor, changes);
+    await applyLSPEdits(editor, changes);
   }
 }
