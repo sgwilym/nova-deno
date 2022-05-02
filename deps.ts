4d4981b033c3623810b1b0073aa6a009cbd66c31
Sam Gwilym <gwilym@me.com>
2022-05-02 10:34:40 +0200

    Use esm.sh instead of skypack for strip-json-comments dependency.

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
 export * as lsp from "https://cdn.skypack.dev/vscode-languageserver-types?dts";
-export { default as stripJSONcomments } from "https://cdn.skypack.dev/strip-json-comments?dts";
+export { default as stripJSONcomments } from "https://esm.sh/strip-json-comments";
